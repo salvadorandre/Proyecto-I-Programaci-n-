5deec7215835f79254b7a96f6f683ba1c95e8d8e
unknown <allanvasquez1312@gmail.com>
2025-04-10 21:38:06 -0600

    Ya se arreglo esa madre

---
+++ estudiantes.h
@@ -0,0 +1,32 @@
+#include<iostream>
+#pragma once
+
+using namespace std;
+
+
+class Estudiante{
+	
+	private:
+		string nombre;
+		int edad;
+		string genero;
+		string grado;
+		float promedio;
+	
+	public:
+		void crearEstudiante(){
+			struct Est{
+				
+			};
+			
+			
+		};
+		
+		void obtenerEstudiantePorID(){
+			
+		}
+		
+		void obtenerEstudiantes(){
+			
+		}
+};
