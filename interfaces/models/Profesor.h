a8af8f74b9f130f523e05e615177fa6d6bef0dae
SEdwinFBI <baquiaxfernando0@gmail.com>
2025-04-10 21:40:28 -0600

    modelo profesor

---
+++ interfaces/models/Profesor.h
@@ -0,0 +1,19 @@
+//
+// Created by Admin on 10/04/2025.
+//
+
+#ifndef PROFESOR_H
+#define PROFESOR_H
+#include <string>
+
+using namespace std;
+
+class Profesor {
+    public:
+      int idProfesor;
+      string nombre;
+};
+
+
+
+#endif //PROFESOR_H
---
+++ src/models/Profesor.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Admin on 10/04/2025.
+//
+
+#include "../../interfaces/models/profesor.h"
+
+
