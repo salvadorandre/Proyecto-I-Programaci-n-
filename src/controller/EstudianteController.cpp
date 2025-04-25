f44073748c3f2129ee585977be8b10f60437e9bd
unknown <allanvasquez1312@gmail.com>
2025-04-24 21:33:47 -0600

    Se agrego el menu para elegir genero y grado de estudiantes

--- src/controller/EstudianteController.cpp
+++ src/controller/EstudianteController.cpp
@@ -3,6 +3,7 @@
 #include "../../interfaces/services/EstudianteService.h"
 #include "../../interfaces/controller/EstudianteController.h"
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -37,6 +38,7 @@ EstudianteController::EstudianteController(EstudianteService estudianteService):
     }
 
     void EstudianteController::crearEstudiantes() {
+        int genero, grado;
         Estudiante estudiante;
 
         cout << "Ingrese un ID para el estudiante" << endl;
@@ -49,11 +51,54 @@ EstudianteController::EstudianteController(EstudianteService estudianteService):
         cin.ignore();
         cout << "Ingrese su nombre" << endl;
         cin.getline(estudiante.nombre, sizeof(estudiante.nombre));
-        cout << "Ingrese el género" << endl;
-        cin.getline(estudiante.genero, sizeof(estudiante.genero));
-        cout << "Ingrese el grado" << endl;
-        cin.getline(estudiante.grado, sizeof(estudiante.grado));
-       estudiante.active = true;
+
+
+        do {
+            cout << "Ingrese el genero:" << endl;
+            cout << "1. Masculino, 2. Femenino" << endl;
+            cin >> genero;
+
+            if (genero == 1) {
+                strcpy(estudiante.genero, "Masculino");
+            }else if (genero == 2) {
+                strcpy(estudiante.genero, "Femenino");
+            }else {
+                cout << "Genero no valido" << endl;
+            }
+
+        }while (genero!=1 && genero!=2);
+
+
+        do {
+            cout << "Ingrese el grado" << endl;
+            cout << "1. 1ro Basico, 2. 2do Basico, 3. 3ro Basico" << endl;
+            cout << "4. 4to Bach, 5. 5to Bach" << endl;
+
+            cin >> grado;
+
+            switch (grado) {
+                case 1:
+                    strcpy(estudiante.grado, "1ro Basico");
+                    break;
+                case 2:
+                    strcpy(estudiante.grado, "2do Basico");
+                    break;
+                case 3:
+                    strcpy(estudiante.grado, "3ro Basico");
+                    break;
+                case 4:
+                    strcpy(estudiante.grado, "4to Bach");
+                    break;
+                case 5:
+                    strcpy(estudiante.grado, "5to Bach");
+                    break;
+                default:
+                    cout << "Grado no valido" << endl;
+                    break;
+            }
+        }while (grado >= 0 || grado >=6);
+
+        estudiante.active = true;
         cout << "Creando estudiante..." << endl;
         estudianteService.crearEstudiante(estudiante);
     }
